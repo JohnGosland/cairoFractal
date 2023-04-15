dc72ddc1b102182f87e139c8a09974054eac3353
John G <johngoslan@gmail.com>
2023-04-15 00:32:44 -0400

    Removed Multi-threads. Revert back to original.

--- cairoFractal.cpp
+++ cairoFractal.cpp
@@ -1,40 +1,50 @@
 #include <cairo.h>
-#include <cmath>
 #include <cairo-svg.h>
+#include <cmath>
 
 const double PI = 3.14159265358979323846;
 
-void branch(cairo_t* cr, double x, double y, double angle, int depth)
+void branch(cairo_t* beetle, double x, double y, double angle, int depth)
 {
-    if (depth == 0) return;
+    if (depth < 1) return;
 
     double x2 = x + std::cos(angle) * depth * 10;
     double y2 = y + std::sin(angle) * depth * 10;
 
-    cairo_move_to(cr, x, y);
-    cairo_line_to(cr, x2, y2);
-    cairo_stroke(cr);
+    cairo_move_to(beetle, x, y);
+    cairo_line_to(beetle, x2, y2);
+    cairo_stroke(beetle);
 
-    branch(cr, x2, y2, angle - 0.4, depth - 1);
-    branch(cr, x2, y2, angle + 0.4, depth - 1);
+    branch(beetle, x2, y2, angle - 0.4, depth - 1);
+    branch(beetle, x2, y2, angle + 0.4, depth - 1);
 }
 
 int main()
 {
-    cairo_surface_t* surface = cairo_svg_surface_create("fractal.svg", 500, 500);
-    cairo_t* cr = cairo_create(surface);
+    
 
-    cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_set_line_width(cr, 1);
+    cairo_surface_t* surface = cairo_svg_surface_create("fractal.svg", 2000, 2000);
+    cairo_t* beetle = cairo_create(surface);
+    cairo_set_source_surface(beetle, surface, 0, 0);
+    cairo_paint(beetle);
+
+    cairo_set_source_rgb(beetle, 0, 0, 0);
+    cairo_set_line_width(beetle, 1);
 
     double x = 250;
     double y = 500;
     double angle = -PI/2;
 
-    branch(cr, x, y, angle, 10);
+    for(int i = 0; i < 4; i++){
+        branch(beetle, x, y, angle + (i * PI * .5), 10);
+    };
+
+    
+
+
 
     cairo_surface_finish(surface);
-    cairo_destroy(cr);
+    cairo_destroy(beetle);
     cairo_surface_destroy(surface);
 
     return 0;
